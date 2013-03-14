53907f255b2a2009dcdf01df9c5935ba3c8728d0
Lukas Bajer <bajel3am@u-pl35.ms.mff.cuni.cz>
2013-03-14 12:56:04 +0100

    empty file for Gibbs sampler, intro comment in modelGetPOI.m

--- src/model/modelGetPOI.m
+++ src/model/modelGetPOI.m
@@ -1,4 +1,9 @@
 function poi = modelGetPOI(M, x, target)
+% modelGetPOI - returns probability of improvement (POI)
+% 
+% poi = modelGetPOI(M, x, target) - returns probability of improvement (POI) for specified @target (usually somewhat better than best f(x) sofar) at @x's from model @M
+%
+% x     vector or matrix of size n by D (n = # of samples, D = dimension)
 
 [m s2] = modelPredict(M, x);
 q = (target - m) ./ sqrt(s2);
---
+++ src/sampler/samplerGibbs.m
@@ -0,0 +1,6 @@
+function s = samplerGibbs(M, lb, ub, target);
+% Gibbs MCMC sampler based on Probability of improvement
+
+% Mock implementation
+return zeros(1, M.dim)
+
