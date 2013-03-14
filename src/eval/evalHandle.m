89eaa088345637e22a1ed8ee23226b4642b5d96f
Viktor Charypar <charypar@gmail.com>
2013-03-14 18:59:57 +0100

    Updated handle evaluator

--- src/eval/evalHandle.m
+++ src/eval/evalHandle.m
@@ -1,7 +1,9 @@
-function y = evalHandle(x, m, s2, params)
+function y = evalHandle(x, m, s2, opts)
 % Simple direct function evaluation
-% x  - input point
-% m  - model prediction
-% s2 - model error estimate
+% x  - input matrix (n by dim)
+% m  - model prediction vector (length dim) - empty for initial evaluation in DoE
+% s2 - model error estimate vector (length dim) - empty for initial evaluatio in DoE
+% opts - options struct, most importantly the handle
+%        handle - function handle taking one arugment - input matrix x - returning an output vector y
 
-y = feval(params.handle, x);
+y = feval(opts.handle, x);
