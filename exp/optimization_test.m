757481d2ab2c2fe1c6e64dabae5ebf30be26986e
Viktor Charypar <charypar@gmail.com>
2013-03-19 16:02:39 +0100

    Split optimization test to 1D and 2D tests

--- exp/optimization_test.m
+++ exp/optimization_test.m
@@ -2,26 +2,18 @@ clear; close all; clc;
 
 opts.popSize = 10;
 % % 1D
-% opts.lowerBound = [-5 -5];
-% opts.upperBound = [ 5  5];
+opts.lowerBound = -5;
+opts.upperBound =  5;
 
-% 2D
-opts.lowerBound = [-5 -5];
-opts.upperBound = [ 5  5];
-
-% opts.eval.handle = @(x) ( sum(x.^2, 2) );
+opts.eval.handle = @(x) ( sum(x.^2, 2) );
 % opts.eval.handle = @rastrigin;
-opts.eval.handle = @sphere;
 
 opts.doe.n = 20; % initial dataset
 opts.sampler.target = 0; % testing on x^2, we know the target
 opts.stop.evaluations = 100;
 
 % % 1D
-% [xopt run] = gpeda(opts, @evalHandle, @doeRandom, @sampleGibbs, [], @stopNEvals, @gpedaStep2d);
-
-% 2D
-[xopt run] = gpeda(opts, @evalHandle, @doeRandom, @sampleGibbs, [], @stopNEvals, @gpedaStep2d);
+[xopt run] = gpeda(opts, @evalHandle, @doeRandom, @sampleGibbs, [], @stopNEvals, @gpedaStep1d);
 
 xopt
 run
