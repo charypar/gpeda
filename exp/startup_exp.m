b033e3155dca37dee4d1097db2a76690c0ffb52f
Lukas Bajer <bajeluk@seznam.cz>
2013-03-25 14:07:26 +0100

    Addpath of exp/ directory

---
+++ exp/startup_exp.m
@@ -0,0 +1,10 @@
+addpath exp/
+addpath exp/bbob/
+addpath exp/bbob/opt/
+addpath exp/bench/   
+addpath exp/tests/
+addpath exp/util/
+addpath exp/vendor/ 
+addpath exp/vendor/bbob/
+addpath exp/vendor/cmaes/  
+
