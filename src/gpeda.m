9f4d51be65ea4da3466da8991634bf4c1b0adc01
Lukas Bajer <bajeluk@seznam.cz>
2013-05-13 17:48:26 +0200

    Non-drastical refactoring

--- src/gpeda.m
+++ src/gpeda.m
@@ -1,4 +1,4 @@
-function [xopt varargout] = gpeda(options, eval, doe, sampler, rescale, restart, stop, varargin)
+function [xopt varargout] = gpeda(options, eval_fcn, doe, sampler, rescale, restart, stop, varargin)
 % Gausian Process based variation of estimation of distribution algorithm (EDA). Performs a
 % heuristic optimization run.
 %
@@ -7,18 +7,18 @@ function [xopt varargout] = gpeda(options, eval, doe, sampler, rescale, restart,
 %           lowerBound - lower bound of the search (piece-wise)
 %           upperBound - upper bound of the search
 %           model      - custom model (see modelInit)
-%           eval       - evaluation strategy options
+%           eval_fcn   - evaluation strategy options
 %           doe        - initial design of experiment
 %           sampler    - sampling strategy options
 %           stop       - stop conditions options
-% eval    - evaluation strategy (function handle)
+% eval_fcn - evaluation strategy (function handle)
 % doe     - initial design of experiment (function handle)
 % sampler - sampling strategy (function handle or cell array of function handles)
 % restart - restart conditions (function handle or cell array of function handles), e.g. stall
 % stop    - termination conditions (function handle or cell array of function handles), e.g. 
 %   evaluation budget depleted
 % 
-% As an pptional argument you can pass in a function handle that will be called in each iteration with
+% As an optional argument you can pass in a function handle that will be called in each iteration with
 % the current state of the run. You can use this to observe and debug the run.
 %
 % Some of the strategies (sampling, restart & stop conditions) can have composite structure: multiple
@@ -39,7 +39,6 @@ maxTrainErrors = 8;
 
 lb = options.lowerBound(:)';
 ub = options.upperBound(:)';
-n  = options.popSize;
 dim = length(lb);
 
 run.options = options;
@@ -49,7 +48,7 @@ run.attempts = {};
 run.notSPDCovarianceErrors = [];
 
 att = 1;
-run.attempts{att} = initAttempt(lb, ub, options);
+run.attempts{att} = initAttempt(lb, ub, eval_fcn, options);
 pop = [];
 
 disp(sprintf('\n ==== Starting new optimization run ==== \n'));
@@ -60,18 +59,16 @@ while ~evalconds(stop, run, options.stop) % until one of the stop conditions occ
   it = run.attempts{att}.iterations;
   ds = run.attempts{att}.dataset;
   M = run.attempts{att}.model;
-  scale = run.attempts{att}.scale;
-  shift = run.attempts{att}.shift;
   pop = []; tolXDistRatio = 0;
 
   disp(sprintf('\n-- Attempt %d, iteration %d --', att, it));
 
   % train model
   disp(['Training model...']);
-  [M_try nErrors] = modelTrain(M, ds.x, ds.y);
-  run.notSPDCovarianceErrors = [run.notSPDCovarianceErrors nErrors];
+  [M_try nTrainErrors] = modelTrain(M, ds.x, ds.y);
+  run.notSPDCovarianceErrors = [run.notSPDCovarianceErrors nTrainErrors];
 
-  if (nErrors > maxTrainErrors) 
+  if (nTrainErrors > maxTrainErrors) 
     disp('Too many errors while training model. Find and evaluate minimum of the GP model.');
     if (~isfield(run.attempts{att}.model, 'dataset'))
       % the actual model does not have any dataset yet, take the very last model
@@ -82,15 +79,7 @@ while ~evalconds(stop, run, options.stop) % until one of the stop conditions occ
     pop = best_x_model;
     % TODO: generate several solutions, not just one
 
-    if (length(run.notSPDCovarianceErrors) > 1  &&  run.notSPDCovarianceErrors(end-1) > maxTrainErrors)
-      % do restart, because last rescale didn't help
-      disp('--> 2nd time! Restart.');
-      doRestart = 1; doRescale = 0;
-    else  % if (nErrors > maxTrainErrors) 
-      % first train error, do rescale
-      disp('--> 1st time, do rescale and we will see.');
-      doRescale = 1; doRestart = 0;
-    end
+    [doRestart, doRescale] = checkNumberOfSPDCovarianceErrors(run.notSPDCovarianceErrors, maxTrainErrors);
   else
     % sample new population
     M = M_try;
@@ -99,23 +88,8 @@ while ~evalconds(stop, run, options.stop) % until one of the stop conditions occ
     try
       [pop tolXDistRatio] = sample(sampler, M, dim, options.popSize, run.attempts{att}, options.sampler);
     catch err
-      disp(['Sample error: ' err.identifier]);
-      disp(getReport(err));
-      if strcmp(err.identifier, 'sampleGibbs:NarrowDataset')
-        doRestart = 1;
-        fprintf('  NarrowDataset: size(pop) = %s; tolXDistRatio = %f\n', num2str(size(pop)), tolXDistRatio);
-      end
-      if strcmp(err.identifier, 'sampleGibbs:CovarianceMatrixNotSPD')
-        doRestart = 1; fprintf(err.message);
-      end
-      if strcmp(err.identifier, 'sampleGibbs:NarrowProbability')
-        doRescale = 1;
-        fprintf('  NarrowProbability: size(pop) = %s; tolXDistRatio = %f\n', num2str(size(pop)), tolXDistRatio);
-      end
-      if strcmp(err.identifier, 'sampleGibbs:NoProbability')
-        doRestart = 1;
-        fprintf('  NoProbability. Restart.\n');
-      end
+      [doRestart, doRescale] = dispatchSampleError(err, tolXDistRatio, doRestart, doRescale);
+      
       [best_x_model best_y_model exflag] = findModelMinimum(M, run.attempts{att});
       disp(['Continuous model minimum: f(' num2str(best_x_model) ') = ' ...
         num2str(best_y_model) ', exflag = ' num2str(exflag)]);
@@ -130,38 +104,21 @@ while ~evalconds(stop, run, options.stop) % until one of the stop conditions occ
 
     run.attempts{att}.populations{it} = pop;
     run.attempts{att}.tolXDistRatios(it) = tolXDistRatio;
-
     run.attempts{att}.model = M;
+
+    % call the "observer", if there were any
     if nargin > 7 && isa(varargin{1}, 'function_handle')
       feval(varargin{1}, run)
     end
 
     % evaluate and add to dataset
     disp(['Evaluating ' num2str(size(pop, 1)) ' new individuals']);
-
     [m s2] = modelPredict(M, pop);
+    x = transform(pop, run.attempts{att}.scale, run.attempts{att}.shift);
+    y = feval(eval_fcn, x, m, s2, options.eval);
 
-    x = transform(pop, scale, shift);
-    y = feval(eval, x, m, s2, options.eval);
-
-    run.attempts{att}.evaluations = run.attempts{att}.evaluations + length(y);
-    run.attempts{att}.bests.evaluations = [run.attempts{att}.bests.evaluations; ...
-      run.attempts{att}.evaluations];
-
-    % store the best so far
-    [ymin i] = min(y);
-    if size(run.attempts{att}.bests.yms2, 1) < 1 || ymin < run.attempts{att}.bests.yms2(end, 1)
-      % record improved solution
-      ev = run.attempts{att}.evaluations;
-      fprintf('Best solution improved to f(%s) = %s. (used %d evaluations in this attempt)\n', num2str(pop(i,:)), num2str(ymin), ev);
-      run.attempts{att}.bests.x(end + 1, :) = pop(i, :);
-      run.attempts{att}.bests.yms2(end + 1, :) = [y(i) m(i) s2(i)];
-    else
-      disp(['Best solution did not improve, still at ' num2str(run.attempts{att}.bests.yms2(end, :))]);
-      % record unbeaten last solution
-      run.attempts{att}.bests.x(end + 1, :) = run.attempts{att}.bests.x(end, :);
-      run.attempts{att}.bests.yms2(end + 1, :) = run.attempts{att}.bests.yms2(end, :);
-    end
+    % update statistics of the number of original evaluations and store the bests
+    run.attempts{att} = updateAttempt(run.attempts{att}, pop, y, m, s2);
 
     % add new samples to the dataset for next iteration
     disp(['Augmenting dataset']);
@@ -182,7 +139,7 @@ while ~evalconds(stop, run, options.stop) % until one of the stop conditions occ
     att = run.attempt;
   
     % initialize new attempt
-    run.attempts{att} = initAttempt(lb, ub, options);
+    run.attempts{att} = initAttempt(lb, ub, eval_fcn, options);
     run.notSPDCovarianceErrors = [];
     doRestart = 0;
   elseif doRescale || isfield(options, 'rescale') && evalconds(rescale, run, options.rescale)
@@ -227,7 +184,7 @@ end
 
 % Support functions
 
-function attempt = initAttempt(re_lb, re_ub, options)
+function attempt = initAttempt(re_lb, re_ub, eval_fcn, options)
   D = length(re_lb);
 
   if(isfield(options, 'model'))
@@ -246,7 +203,7 @@ function attempt = initAttempt(re_lb, re_ub, options)
   dsx = attempt.dataset.x;
   dsx = transform(dsx, attempt.scale, attempt.shift);
   
-  attempt.dataset.y = feval(eval, dsx, [], [], options.eval);
+  attempt.dataset.y = feval(eval_fcn, dsx, [], [], options.eval);
   attempt.evaluations = length(attempt.dataset.y);
   
   % FIXME assumes mean function is a constant
@@ -394,5 +351,66 @@ function [pop fval exflag] = findModelMinimum(model, thisAttempt)
     'Display', 'off');
   [pop fval exflag] = fminsearch(gp_predict, thisAttempt.bests.x(end,:), fminoptions);
 end
+
+function [doRestart, doRescale] = checkNumberOfSPDCovarianceErrors(notSPDCovarianceErrors, maxTrainErrors)
+% checkes whether restart of rescale should happen according to SPD covarnace errors
+% in this and privous run
+  if (length(notSPDCovarianceErrors) > 1  &&  notSPDCovarianceErrors(end-1) > maxTrainErrors)
+    % do restart, because last rescale didn't help
+    disp('--> 2nd time! Restart.');
+    doRestart = 1; doRescale = 0;
+  else  % if (nTrainErrors > maxTrainErrors) 
+    % first train error, do rescale
+    disp('--> 1st time, do rescale and we will see.');
+    doRescale = 1; doRestart = 0;
+  end
+end
+
+function [doRestart, doRescale] = dispatchSampleError(err, tolXDistRatio, doRestart, doRescale)
+% indentifies different errors comming from sample()
+% prints a warning message and decides whether rescale or restart should happen
+  disp(['Sample error: ' err.identifier]);
+  disp(getReport(err));
+  if strcmp(err.identifier, 'sampleGibbs:NarrowDataset')
+    doRestart = 1;
+    fprintf('  NarrowDataset: size(pop) = %s; tolXDistRatio = %f\n', num2str(size(pop)), tolXDistRatio);
+  end
+  if strcmp(err.identifier, 'sampleGibbs:CovarianceMatrixNotSPD')
+    doRestart = 1; fprintf(err.message);
+  end
+  if strcmp(err.identifier, 'sampleGibbs:NarrowProbability')
+    doRescale = 1;
+    fprintf('  NarrowProbability: size(pop) = %s; tolXDistRatio = %f\n', num2str(size(pop)), tolXDistRatio);
+  end
+  if strcmp(err.identifier, 'sampleGibbs:NoProbability')
+    doRestart = 1;
+    fprintf('  NoProbability. Restart.\n');
+  end
+end
+
+
+function thisAttempt = updateAttempt(thisAttempt, pop, y, m, s2);
+% update statistics of the number of original evaluations
+
+  thisAttempt.evaluations = thisAttempt.evaluations + length(y);
+  thisAttempt.bests.evaluations = [thisAttempt.bests.evaluations; ...
+    thisAttempt.evaluations];
+
+  % store the best so far
+  [ymin i] = min(y);
+  if size(thisAttempt.bests.yms2, 1) < 1 || ymin < thisAttempt.bests.yms2(end, 1)
+    % record improved solution
+    ev = thisAttempt.evaluations;
+    fprintf('Best solution improved to f(%s) = %s. (used %d evaluations in this attempt)\n', num2str(pop(i,:)), num2str(ymin), ev);
+    thisAttempt.bests.x(end + 1, :) = pop(i, :);
+    thisAttempt.bests.yms2(end + 1, :) = [y(i) m(i) s2(i)];
+  else
+    disp(['Best solution did not improve, still at ' num2str(thisAttempt.bests.yms2(end, :))]);
+    % record unbeaten last solution
+    thisAttempt.bests.x(end + 1, :) = thisAttempt.bests.x(end, :);
+    thisAttempt.bests.yms2(end + 1, :) = thisAttempt.bests.yms2(end, :);
+  end
+end
+
 
 end
