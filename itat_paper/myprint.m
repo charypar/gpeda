257b96b72e8788af30472f33c32e06fc7829ac9e
Lukas Bajer <bajeluk@seznam.cz>
2013-07-24 23:00:02 +0200

    Final version of ITAT2013 article (ver. 03)

--- itat_paper/myprint.m
+++ itat_paper/myprint.m
@@ -9,14 +9,13 @@ function myprint(filename, fhandle)
   set(gcf,'PaperUnits','centimeters')
   set(gcf,'PaperSize',[10 9.2]);
   % for wide pictures:
-  % set(gcf,'PaperSize',[10 19]);
+  % set(gcf,'PaperSize',[18 12]);
   set(gcf,'PaperOrientation','portrait')
   % set(gcf,'PaperPosition',[0 4 14 10]);
   % set(gcf,'PaperPosition',[0.5 4.5 13 9]);
-  % for wide pictures:
   set(gcf,'PaperPosition',[-0.5 -0.4 11 10]);
+  % for wide pictures:
+  % set(gcf,'PaperPosition',[-0.7 -0.4 20 12.5]);
   print(fhandle, '-dpdf', [filename '.pdf']);
   print(fhandle, '-dpng', [filename '_.png']);
   print(fhandle, '-deps', [filename '.eps']);
-
-
