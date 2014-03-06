06906110d6a10059ecefca997d321ded1383b86d
Lukas Bajer <bajeluk@seznam.cz>
2014-03-06 19:54:30 +0100

    Optimization toolbox fmincon() instead of Rasmussen's minimize() - first version

--- src/model/modelTrain.m
+++ src/model/modelTrain.m
@@ -13,6 +13,32 @@ global modelTrainNErrors;
 model = struct(M); % copy the original model
 model.dataset.x = x; model.dataset.y = y; % store dataset
 
+hyp = model.hyp;
+f = @(par) linear_gp(par, hyp, @infExactCountErrors, model.meanfunc, model.covfunc, model.likfunc, x, y);
+
+linear_hyp = unwrap(hyp)';
+l_cov = length(hyp.cov);
+lb = [zeros(1,l_cov) -Inf * ones(1,length(linear_hyp)-l_cov)];
+ub = [20 * ones(1,l_cov) Inf * ones(1,length(linear_hyp)-l_cov)];
+options = optimset('GradObj', 'on', ...
+        'TolFun', 1e-8, ...
+        'TolX', 1e-8, ...
+        'MaxIter', 1000, ...
+        'MaxFunEvals', 1000, ...
+        'Display', 'final');
+disp('Model training...');
+opt = fmincon(f, linear_hyp', [], [], [], [], lb, ub, [], options);
+
+model.hyp = rewrap(hyp, opt);
+modelTrainNErrors = 0;
+nErrors = modelTrainNErrors;
+Mout = model;
+
+% DEBUG OUTPUT:
+fprintf('Final hyperparameters: ');
+disp(model.hyp);
+
+%{
 modelTrainNErrors = 0;
 % model.hyp = minimize(model.hyp, @gp, -100, model.inffunc, model.meanfunc, model.covfunc, model.likfunc, x, y);
 model.hyp = minimize(model.hyp, @gp, -100, @infExactCountErrors, model.meanfunc, model.covfunc, model.likfunc, x, y);
@@ -20,6 +46,9 @@ model.hyp = minimize(model.hyp, @gp, -100, @infExactCountErrors, model.meanfunc,
 
 nErrors = modelTrainNErrors;
 Mout = model;
+%}
+
+end
 
 function [post nlZ dnlZ] = infExactCountErrors(hyp, mean, cov, lik, x, y)
   try
@@ -30,4 +59,8 @@ function [post nlZ dnlZ] = infExactCountErrors(hyp, mean, cov, lik, x, y)
   end
 end
 
+function [nlZ dnlZ] = linear_gp(linear_hyp, s_hyp, inf, mean, cov, lik, x, y)
+  hyp = rewrap(s_hyp, linear_hyp');
+  [nlZ s_dnlZ] = gp(hyp, inf, mean, cov, lik, x, y);
+  dnlZ = unwrap(s_dnlZ)';
 end
